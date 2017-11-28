0537ef9922a58a9d36e0d440c119a9ad2d4fde8a
Eugeny Burnashov <shamcode@gmail.com>
2017-11-28 12:12:48 +0700

    Revert

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require( 'webpack' );
+
+const env = process.env.WEBPACK_ENV;
+const plugins = [
+    new webpack.NoEmitOnErrorsPlugin()
+];
+
+//if ( env === 'build' ) {
+//    plugins.push( new webpack.optimize.UglifyJsPlugin( { minimize: true } ) );
+//}
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'index.js',
+        publicPath: '/'
+    },
+    plugins: plugins,
+    module: {
+        loaders: [ {
+            test: /(\.js)$/,
+            loader: 'babel-loader',
+            exclude: /(node_modules)/,
+            include: __dirname
+        }, {
+            test: /\.sht/,
+            loader: 'sham-ui-templates-loader'
+        } ]
+    }
+};
